5b86e41a4f73e3705fcac3db49687a633d5ac903
LakshmiMansi09 <150318091+LakshmiMansi09@users.noreply.github.com>
2024-05-01 23:40:04 +0530

    Add files via upload

---
+++ day-1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+#include<cmath>
+
+int main()
+{
+        int luckyNum[] = { 1, 2, 3, 4 };
+        int b = luckyNum[0] * luckyNum[1];
+        cout << b;
+
+return 0;
+
+}
